d4dacd68045cf76f92f7efef5a86d14285307e4f
SvenGege <su.moonfy@gmail.com>
2018-01-06 01:38:33 +0100

    Update youku.rb

--- lib/onebox/youku.rb
+++ lib/onebox/youku.rb
@@ -1,7 +1,7 @@
 module ::YoukuOneboxOverride
   def to_html
     if SiteSetting.zh_l10n_http_onebox_override
-      "<iframe width='480' height='270' src='http://player.youku.com/embed/#{video_id}' frameborder='0' allowfullscreen></iframe>"
+      "<iframe width='480' height='270' src='https://player.youku.com/embed/#{video_id}' frameborder='0' allowfullscreen></iframe>"
     else
       super
     end
